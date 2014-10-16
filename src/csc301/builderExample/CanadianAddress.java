ef6e9f646b43e1aaa22d500aa6846674d8e88f3d
Joey Freund <joey@cs.toronto.edu>
2014-10-16 11:24:59 -0400

    Cleaning up

--- src/csc301/builderExample/CanadianAddress.java
+++ src/csc301/builderExample/CanadianAddress.java
@@ -1,14 +1,59 @@
 package csc301.builderExample;
 
-public interface CanadianAddress {
 
-	// Question: Why are we using String for apartment and street number? 
-	public String getApartmentNumber();
-	public String getStreetNumber();
-	public String getStreetName();
-	public String getStreetType();
-	public String getCity();
-	public String getProvince();
-	public String getPostalCode();
+public class CanadianAddress {
+
 	
+	private String apartmentNumber;
+	private String streetNumber;
+	private String streetName;
+	private String streetType;
+	private String city;
+	private String province;
+	private String postalCode;
+	
+	
+	
+	public CanadianAddress(String apartmentNumber, String streetNumber,
+			String streetName, String streetType, String city, String province,
+			String postalCode) {
+		this.apartmentNumber = apartmentNumber;
+		this.streetNumber = streetNumber;
+		this.streetName = streetName;
+		this.streetType = streetType;
+		this.city = city;
+		this.province = province;
+		this.postalCode = postalCode;
+	}
+
+	
+	
+	public String getApartmentNumber() {
+		return apartmentNumber;
+	}
+	
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+	
+	public String getStreetName() {
+		return streetName;
+	}
+	
+	public String getStreetType() {
+		return streetType;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public String getProvince() {
+		return province;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
 }

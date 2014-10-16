486a2ed1be05d9c52853458775c9c8e03e086f6e
Joey Freund <joey@cs.toronto.edu>
2014-10-16 11:28:42 -0400

    Cleaning up

--- src/csc301/builderExample/CanadianAddress.java
+++ src/csc301/builderExample/CanadianAddress.java
@@ -1,14 +1,51 @@
 package csc301.builderExample;
 
-public interface CanadianAddress {
 
-	// Question: Why are we using String for apartment and street number? 
-	public String getApartmentNumber();
-	public String getStreetNumber();
-	public String getStreetName();
-	public String getStreetType();
-	public String getCity();
-	public String getProvince();
-	public String getPostalCode();
+/**
+ * Starter code.
+ * Some of the implementation is missing.
+ */
+public class CanadianAddress {
+
 	
+	// Question: Why are we using String for apartment and street number?
+	private String apartmentNumber;
+	private String streetNumber;
+	private String streetName;
+	private String streetType;
+	private String city;
+	private String province;
+	private String postalCode;
+	
+	
+	
+	
+	public String getApartmentNumber() {
+		return apartmentNumber;
+	}
+	
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+	
+	public String getStreetName() {
+		return streetName;
+	}
+	
+	public String getStreetType() {
+		return streetType;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public String getProvince() {
+		return province;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
 }

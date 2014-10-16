52d4af5e70de0d04b0f0b93a965e5e4959d75ca6
Joey Freund <joey@cs.toronto.edu>
2014-10-16 11:18:41 -0400

    Final Builder implementation

--- src/csc301/builderExample/CanadianAddress.java
+++ src/csc301/builderExample/CanadianAddress.java
@@ -1,14 +1,116 @@
 package csc301.builderExample;
 
-public interface CanadianAddress {
 
-	// Question: Why are we using String for apartment and street number? 
-	public String getApartmentNumber();
-	public String getStreetNumber();
-	public String getStreetName();
-	public String getStreetType();
-	public String getCity();
-	public String getProvince();
-	public String getPostalCode();
+public class CanadianAddress {
 	
+	
+	public static class Builder {
+
+		private String apartmentNumber;
+		private String streetNumber;
+		private String streetName;
+		private String streetType;
+		private String city;
+		private String province;
+		private String postalCode;
+		
+		
+		public Builder apartmentNumber(String apartmentNumber){
+			this.apartmentNumber = apartmentNumber;
+			return this;
+		}
+		
+		public Builder streetNumber(String streetNumber){
+			this.streetNumber = streetNumber;
+			return this;
+		}
+		
+		public Builder streetName(String streetName){
+			this.streetName = streetName;
+			return this;
+		}
+		
+		public Builder streetType(String streetType){
+			this.streetType = streetType;
+			return this;
+		}
+		
+		public Builder city(String city){
+			this.city = city;
+			return this;
+		}
+		
+		public Builder province(String province){
+			this.province = province;
+			return this;
+		}
+		
+		public Builder postalCode(String postalCode){
+			this.postalCode = postalCode;
+			return this;
+		}
+		
+		
+		public CanadianAddress build(){
+			// We can validate the arguments, before creating a CanadianAddress instance. 
+			return new CanadianAddress(this);
+		}
+		
+	}
+	
+	
+	
+
+	
+	private String apartmentNumber;
+	private String streetNumber;
+	private String streetName;
+	private String streetType;
+	private String city;
+	private String province;
+	private String postalCode;
+	
+	
+	
+	private CanadianAddress(Builder builder) {
+		this.apartmentNumber = builder.apartmentNumber;
+		this.streetNumber = builder.streetNumber;
+		this.streetName = builder.streetName;
+		this.streetType = builder.streetType;
+		this.city = builder.city;
+		this.province = builder.province;
+		this.postalCode = builder.postalCode;
+	}
+	
+	
+
+
+	public String getApartmentNumber() {
+		return apartmentNumber;
+	}
+
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public String getStreetType() {
+		return streetType;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
 }
--- src/csc301/builderExample/Main.java
+++ src/csc301/builderExample/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		CanadianAddress a = new CanadianAddressBuilder().apartmentNumber("1A")
+		CanadianAddress a = new CanadianAddress.Builder().apartmentNumber("1A")
 				.streetNumber("123-2").streetName("Main")
 				.streetType("Ave").city("Toronto").province("ON")
 				.postalCode("M2K 7R1").build();
